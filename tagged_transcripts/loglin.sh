e0fa6d810cd7d309d9d3dc68885ab14d71d7ad2b
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-09-15 10:07:12 -0400

    ok

--- tagged_transcripts/loglin.sh
+++ tagged_transcripts/loglin.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# get games in connl format
 python games2connl.py
 
+# run the ark tagger
 find games/*connl | grep -v ark | parallel -j 5 --eta "./ark-tweet-nlp-0.3.2/runTagger.sh {} > {}.ark"
+find games/*ark | parallel 'python arc_postproc.py -fn {} -K 5 -phrases'
